e75c4d2fe4a65c85d423543b7fe89180769ed5a2
Serdar Tumgoren <zstumgoren@gmail.com>
2013-04-07 14:33:57 -0700

    comment delete

--- dashboard/apps/hub/static/admin/js/CopyButtonHandler.js
+++ dashboard/apps/hub/static/admin/js/CopyButtonHandler.js
@@ -2,7 +2,6 @@
     $(document).ready(function() {
         $('a.grp-copy-handler').each(function() {
 
-            // Unbind the original click event?
             $(this).click(function(e) {
                 var target = (e.target) ? e.target : e.srcElement;
                 OPELEC.inlines.copy(target);
