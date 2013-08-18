2ec3bdc7d3f1e8a6066be0609791d858cb0c9772
Derek Willis <dwillis@gmail.com>
2013-08-18 12:44:39 -0400

    added state & year checks to metadata api

--- dashboard/apps/hub/views.py
+++ dashboard/apps/hub/views.py
@@ -1,8 +1,14 @@
-from dashboard.apps.hub.models import Election
-from django.http import HttpResponse
+from dashboard.apps.hub.models import Election, State
+from django.http import HttpResponse, Http404
+from django.shortcuts import get_object_or_404
 from django.utils import simplejson
 
 def elections_for_state_and_year(request, state, year):
-    response = simplejson.dumps([{'id': e.slug, 'year': year, 'start_date': e.start_date.strftime('%Y-%m-%d'), 'end_date': e.end_date.strftime('%Y-%m-%d'), 'division': e.division, 'result_type': e.result_type, 'election_type': e.race_type, 'special': e.special, 'offices': e.offices_for_api, 'reporting_levels': e.reporting_levels, 'absentee_provisional': e.absentee_and_provisional, 'note': e.level_note, 'source_url' : e.direct_link, 'portal_url' : e.portal_link} for e in Election.objects.filter(start_date__year=year, state_id=state.upper()).order_by('-start_date')])
-    return HttpResponse(response, content_type="application/json")
-    
+    st = get_object_or_404(State, postal=state.upper())
+    if int(year) < 2000:
+        raise Http404
+    else:
+        elections = Election.objects.filter(start_date__year=year, state_id=st.postal).order_by('-start_date')
+        details = [{'id': e.slug, 'start_date': e.start_date.strftime('%Y-%m-%d'), 'end_date': e.end_date.strftime('%Y-%m-%d'), 'result_type': e.result_type, 'election_type': e.race_type, 'special': e.special, 'offices': e.offices_for_api, 'reporting_levels': e.reporting_levels, 'absentee_provisional': e.absentee_and_provisional, 'note': e.level_note, 'source_url' : e.direct_link, 'portal_url' : e.portal_link} for e in elections ]
+        return HttpResponse(simplejson.dumps({'division': elections[0].division, 'state': st.postal, 'year': year, 'elections': details}), content_type="application/json")
+        
