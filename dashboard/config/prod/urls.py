8e6d47b8ccb81091377186d493812e6dc4b163fe
Serdar Tumgoren <zstumgoren@gmail.com>
2013-08-25 17:17:21 -0700

    add versioning to api

--- dashboard/config/prod/urls.py
+++ dashboard/config/prod/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls.defaults import patterns, include, url
 from dashboard.config.base.urls import urlpatterns
 
 urlpatterns += patterns('dashboard.apps.hub.views',
-    url(r'^api/state/(?P<state>[a-z]+)/year/(?P<year>\d{4})/$', 'elections_for_state_and_year'),
+    url(r'^api/v1/state/(?P<state>[a-z]+)/year/(?P<year>\d{4})/$', 'elections_for_state_and_year'),
 )
