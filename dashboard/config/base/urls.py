7989e03f92c617d5b15858ae5582e7875a0ecacf
Derek Willis <dwillis@gmail.com>
2013-12-07 21:20:46 -0500

    removed original api routes and view

--- dashboard/config/base/urls.py
+++ dashboard/config/base/urls.py
@@ -2,7 +2,6 @@ from django.conf.urls.defaults import patterns, include, url
 from django.contrib import admin
 from tastypie.api import Api
 from dashboard.apps.hub import api
-from dashboard.apps.hub.views import elections_for_state_and_year
 
 admin.autodiscover()
 
@@ -14,6 +13,5 @@ v1_api.register(api.StateResource())
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^grappelli/', include('grappelli.urls')),
-    url(r'^api/v1/state/(?P<state>[a-z]+)/year/(?P<year>\d{4})/$', elections_for_state_and_year),
     url(r'^api/', include(v1_api.urls)),
 )
