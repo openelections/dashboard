b232df64eeefa3e1a3b14e05fefed986730e7d4d
Derek Willis <dwillis@gmail.com>
2019-09-07 09:51:52 -0400

    removed whitenoise

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,3 @@
 from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
 
 application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
