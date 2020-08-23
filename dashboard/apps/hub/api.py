df7fc20b193f7de3f27f6019d1a8e22076b68c93
Derek Willis <dwillis@gmail.com>
2020-08-23 14:07:07 -0400

    turn api back on

--- dashboard/apps/hub/api.py
+++ dashboard/apps/hub/api.py
@@ -1,63 +1,63 @@
-# import re
-#
-# from tastypie import fields
-# from tastypie.resources import ModelResource, ALL, ALL_WITH_RELATIONS
-# from dashboard.apps.hub.models import Election, State, Organization
-#
-#
-# class OrganizationResource(ModelResource):
-#
-#     class Meta:
-#         queryset = Organization.objects.all()
-#         allowed_methods = ['get']
-#         include_resource_ur = False
-#         excludes = ['description']
-#         filtering = {
-#             'name': ['exact', 'iexact'],
-#             'slug': ['exact', 'iexact'],
-#             'gov_level': ['exact', 'iexact'],
-#             'state': ['exact', 'iexact'],
-#         }
-#
-#
-# class StateResource(ModelResource):
-#
-#     class Meta:
-#         queryset = State.objects.all()
-#         allowed_methods = ['get']
-#         include_resource_ur = False
-#         fields = ['postal', 'name']
-#         filtering = {
-#             'name': ALL,
-#             'postal': ['iexact', 'exact'],
-#         }
-#
-#
-# class ElectionResource(ModelResource):
-#
-#     organization = fields.ForeignKey(OrganizationResource,'organization', full=True)
-#     state = fields.ForeignKey(StateResource, 'state', full=True)
-#
-#     class Meta:
-#         queryset = Election.objects.all()
-#         allowed_methods = ['get']
-#         excludes = [
-#             'created',
-#             'modified',
-#             'user',
-#             'level_note',
-#             'note',
-#             'needs_review',
-#         ]
-#         filtering = {
-#             'state': ALL_WITH_RELATIONS,
-#             'organization': ALL_WITH_RELATIONS,
-#             'race_type': ALL,
-#             'start_date': ALL,
-#             'end_date': ALL,
-#         }
-#
-#     def dehydrate_direct_links(self, bundle):
-#         urls = re.sub(r'\n+', "\n", bundle.data['direct_links'].replace('\r', '')).split("\n")
-#         bundle.data['direct_links'] = [url for url in urls if url.strip()]
-#         return bundle.data['direct_links']
+import re
+
+from tastypie import fields
+from tastypie.resources import ModelResource, ALL, ALL_WITH_RELATIONS
+from dashboard.apps.hub.models import Election, State, Organization
+
+
+class OrganizationResource(ModelResource):
+
+    class Meta:
+        queryset = Organization.objects.all()
+        allowed_methods = ['get']
+        include_resource_ur = False
+        excludes = ['description']
+        filtering = {
+            'name': ['exact', 'iexact'],
+            'slug': ['exact', 'iexact'],
+            'gov_level': ['exact', 'iexact'],
+            'state': ['exact', 'iexact'],
+        }
+
+
+class StateResource(ModelResource):
+
+    class Meta:
+        queryset = State.objects.all()
+        allowed_methods = ['get']
+        include_resource_ur = False
+        fields = ['postal', 'name']
+        filtering = {
+            'name': ALL,
+            'postal': ['iexact', 'exact'],
+        }
+
+
+class ElectionResource(ModelResource):
+
+    organization = fields.ForeignKey(OrganizationResource,'organization', full=True)
+    state = fields.ForeignKey(StateResource, 'state', full=True)
+
+    class Meta:
+        queryset = Election.objects.all()
+        allowed_methods = ['get']
+        excludes = [
+            'created',
+            'modified',
+            'user',
+            'level_note',
+            'note',
+            'needs_review',
+        ]
+        filtering = {
+            'state': ALL_WITH_RELATIONS,
+            'organization': ALL_WITH_RELATIONS,
+            'race_type': ALL,
+            'start_date': ALL,
+            'end_date': ALL,
+        }
+
+    def dehydrate_direct_links(self, bundle):
+        urls = re.sub(r'\n+', "\n", bundle.data['direct_links'].replace('\r', '')).split("\n")
+        bundle.data['direct_links'] = [url for url in urls if url.strip()]
+        return bundle.data['direct_links']
