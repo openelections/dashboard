9aae92540319663d7bf2646c0c1bd0544be4e19d
Derek Willis <dwillis@gmail.com>
2020-08-23 11:22:34 -0400

    use prod settings

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,4 @@
 import os
 from django.core.wsgi import get_wsgi_application
-os.environ.setdefault("DJANGO_SETTINGS_MODULE","dashboard.config.base.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE","dashboard.config.prod.settings")
 application = get_wsgi_application()
